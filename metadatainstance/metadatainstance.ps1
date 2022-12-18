f27b5d8011d9aec9bdf0bf32a2f3552ccf585f5e
GauravPandey3723 <120821050+GauravPandey3723@users.noreply.github.com>
2022-12-18 12:47:56 +0530

    Add files via upload

---
+++ metadatainstance/metadatainstance.ps1
@@ -0,0 +1,2 @@
+$metadata=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+$JSON = $metadata | ConvertTo-Json
